0b72ba8898b7d6e70e084dcb29eaaf5ac1ce2fb8
Haley Lock <haleylock@Haleys-MacBook-Air.local>
2018-01-18 09:50:29 -0500

    recipelist

---
+++ cookery/src/components/RecupeList2.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+       
+        <Text style={styles.bigred}>Recipe Name:</Text>
+        <Text style={styles.biggreen}>Prep time:</Text>
+        <Text style={styles.biggreen}>Cook time:</Text>
+        <Text style={[styles.biggreen]}>Serving:</Text>
+         
+
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    bigred: {
+    color: 'black',
+    fontSize: 25,
+    fontWeight: 'bold',
+    height: 60,
+    paddingTop:20
+  
+    },
+    biggreen: {
+    color: 'black',
+    fontSize: 20,
+    height: 60,
+    paddingTop:10
+    },
+
+ 
+});
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+
+
